6c100459fffa420727b005f8e301888b31f5bc61
Greg <greg@yhathq.com>
2013-11-12 12:52:19 -0500

    adding create table script

---
+++ nba-win-probability/test/create_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE os_nba_games (
+    _id bigint NOT NULL,
+    team_home character varying,
+    team_away character varying,
+    time_remaining integer,
+    home_lead integer,
+    game_id character varying,
+    gamedate character varying,
+    insert_time timestamp without time zone DEFAULT now()
+);
